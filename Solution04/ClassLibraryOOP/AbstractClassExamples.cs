752d07275e9292c1f3b8ccd8b169fbdc0754c5e8
Aghasi Lorsabyan <lorsabyan@gmail.com>
2017-01-09 16:05:39 +0400

    Added lesson 09.

---
+++ Solution04/ClassLibraryOOP/AbstractClassExamples.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryOOP
+{
+    public abstract class AbstractClass
+    {
+        public abstract void DoWork();
+
+        public void DoAnotherWork()
+        {
+
+        }
+
+        public virtual void F()
+        {
+            
+        }
+
+        public int x;
+        public int Y { get; set; }
+        public abstract int Z { get; set; }
+
+        public AbstractClass()
+        {
+
+        }
+
+        public AbstractClass(int x)
+        {
+            this.x = x;
+        }
+
+        // Wrong syntax
+        //public abstract AbstractClass(int x, int y, int z);
+    }
+
+    public class InheritedClass1 : AbstractClass
+    {
+        public override int Z
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Solution04/ClassLibraryOOP/InterfaceExamples.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryOOP1
+{
+    public class BaseClass { }
+
+    public interface IMyInterface
+    {
+        int MyProperty { get; set; }
+        void DoWork();
+    }
+
+    public interface IMyInterface1
+    {
+
+    }
+
+    public class ClassWithInterfaces : BaseClass, IMyInterface, IMyInterface1
+
+    {
+        public int MyProperty
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface I
+    {
+        void F();
+        int X { get; set; }
+    }
+
+    public class A : I
+    {
+        public int Y { get; set; }
+        public void F1()
+        {
+
+        }
+
+        public int X
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void F()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class B : I
+    {
+        public int Z { get; set; }
+        public void F2()
+        {
+
+        }
+
+        public int X
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void F()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Program
+    {
+        public void Main()
+        {
+            var a = new A();
+            var b = new B();
+
+            DoWork(a);
+            DoWork(b);
+        }
+
+        public void DoWork (I value)
+        {
+            Console.WriteLine(value.X);
+            value.F();
+        }
+    }
+}
