b9a1a79d632552436955c66ea6460ffdef636d55
Aghasi Lorsabyan <lorsabyan@gmail.com>
2017-02-04 14:27:37 +0400

    Added Lesson 24.

---
+++ Solution11/ConsoleApplicationTaskFactory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTaskFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run(() =>
+            {
+
+            });
+            TaskTest();
+
+            DoWork();
+        }
+
+        [Obsolete("This function is depricated. Please use F function instead.", false)]
+        private static async void TaskTest()
+        {
+            var task1 = new Task(() => { });
+            task1.Start();
+            await task1;
+        }
+
+        
+    }
+}
