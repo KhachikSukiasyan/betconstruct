fcea4ea83da36862e358a16bc41fe8e75f7a68a6
Aghasi Lorsabyan <lorsabyan@gmail.com>
2017-04-11 14:11:46 +0400

    Added WPF Sample

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Windows;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGetHtml_Click(object sender, RoutedEventArgs e)
+        {
+            var url = textUrl.Text;
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    btnGetHtml.IsEnabled = false;
+                    textHtml.Text = await client.GetStringAsync(url);
+                }
+                catch
+                {
+                    textHtml.Text = "Oops! Something went wrong.";
+                }
+                finally
+                {
+                    btnGetHtml.IsEnabled = true;
+                }
+            }
+        }
+    }
+}
